d141e0a8e1411ac391aeb22b47b1cf22828c2a87
matt32106 <git.m7@xoxy.net>
2014-05-20 17:08:21 +0200

    edited comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,25 @@
 ## Below are two functions that are used to create a special object that 
 ## stores a matrix and caches its inverse.
 
+## example for testing the functions:
+## > source("cachematrix.R")
+## > a<-matrix(c(0,2,2,0), nrow = 2, ncol = 2)
+## > b<-makeCacheMatrix(a)
+## > cacheSolve(b)
+##       [,1] [,2]
+## [1,]  0.0  0.5
+## [2,]  0.5  0.0
+## > cacheSolve(b)
+## getting cached data
+##       [,1] [,2]
+## [1,]  0.0  0.5
+## [2,]  0.5  0.0
+##
+## note the getting cached data message the second time 
+## the cacheSolve function is called
+
+
+
 ## makeCacheMatrix creates a special list containing a function to
 ##   set the value of the matrix
 ##   get the value of the matrix
